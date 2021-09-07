cfa37de2c2003b08d27a2e624db4f1e44d012a4e
Victor Dualibi <victor.dualibi@neoway.com.br>
2021-09-07 17:13:05 -0300

    fix: changed df column number from 10 to 7

--- tests/test_features.py
+++ tests/test_features.py
@@ -17,7 +17,7 @@ def test_type_target_diff_transformer(sample_input_data):
 def test_shape_target_diff_transformer(sample_input_data):
     transformer = features.TargetDiffTransformer(*config.model_config.diff_create_var)
 
-    expected_df_shape = (49, 10)
+    expected_df_shape = (49, 7)
     subject = transformer.fit_transform(sample_input_data)
 
     assert subject.shape == expected_df_shape
@@ -44,7 +44,7 @@ def test_type_fake_var_transformer(sample_input_data):
 def test_shape_fake_var_transformer(sample_input_data):
     transformer = features.FakeVariableTransformer(*config.model_config.fake_create_var)
 
-    expected_df_shape = (49, 10)
+    expected_df_shape = (49, 7)
     subject = transformer.fit_transform(sample_input_data)
 
     assert subject.shape == expected_df_shape
